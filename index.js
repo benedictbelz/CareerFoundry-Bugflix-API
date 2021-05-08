117035065eb9b09b6a59e296f2177e02c4087399
Benedict Belz <info@benedictbelz.eu>
2021-05-08 16:53:06 +0200

    2.5: REST & API Endpoints

--- index.js
+++ index.js
@@ -12,25 +12,47 @@ app.use((err, req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-	res.send('Welcome to Bugflix');
+	res.status(200).send('Welcome to Bugflix');
 });
 
 app.get('/movies', (req, res) => {
-	res.json({
-		top10: [
-			'Apokalypse Now',
-			'Inception',
-			'Children of Men',
-			'No Counrty for Old Men',
-			'City of God',
-			'Hero',
-			'Waking Life',
-			'Minority Report',
-			'Fight Club',
-			'Pulp Fiction',
-			'Joker',
-		],
-	});
+	res.status(200).send('Here is the list of all movies.');
+});
+
+app.get('/movies/:name', (req, res) => {
+	res.status(200).send('Here is all information about this movie.');
+});
+
+app.get('/genres/:name', (req, res) => {
+	res.status(200).send('Here is all information about this genre.');
+});
+
+app.get('/directors/:name', (req, res) => {
+	res.status(200).send('Here is all information about this director.');
+});
+
+app.post('/users', (req, res) => {
+	res.status(201).send('Your account has been successfully created.');
+});
+
+app.put('/users/:name', (req, res) => {
+	res.status(201).send('Your account has been successfully updated.');
+});
+
+app.delete('/users/:name', (req, res) => {
+	res.status(200).send('Your account has been successfully removed.');
+});
+
+app.post('/users/:name/favourites/:movie', (req, res) => {
+	res.status(201).send('The movie has been successfully added to your favourites list.');
+});
+
+app.delete('/users/:name/favourites/:movie', (req, res) => {
+	res.status(200).send('The movie has been successfully removed from your favourites list.');
+});
+
+app.get('/teapot', (req, res) => {
+	res.status(418).send('I am a teapot.');
 });
 
 app.listen(8080, () => {
